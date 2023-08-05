4063d672ddb2161d7dddb1caa08c2cab917de95a
Sourcery AI <>
2023-08-05 18:16:52 +0000

    'Refactored by Sourcery'

--- examples/syscall.py
+++ examples/syscall.py
@@ -4,7 +4,7 @@ import time
 rop = Exrop("libc.so.6")
 t = time.mktime(time.gmtime())
 rop.find_gadgets(cache=True) # it's slow for first analyze keep waiting
-print("Analyzing done in {}s".format(time.mktime(time.gmtime()) - t))
+print(f"Analyzing done in {time.mktime(time.gmtime()) - t}s")
 chain = rop.syscall(1, (1,2,3))
 chain.dump()
 #print(chain.payload_str())
